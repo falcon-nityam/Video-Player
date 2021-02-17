8cd6c4637aa79e8d405368d9acb6496f0f634ffe
falcon-nityam <nityambhangad0213@gmail.com>
2021-02-17 21:25:01 +0530

    UI created

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-
+const video=document.querySelector('video')
+const progressRange=document.querySelector('.progress-range')
+const progressBar=document.querySelector('.progress-bar')
+const playBtn=document.querySelector('#play-btn')
+const volumeIcon=document.querySelector('.volume-icon')
+const volumeRange=document.querySelector('.volume-range')
+const volumeBar=document.querySelector('.volume-bar')
+const currentTime=document.querySelector('.time-elapsed')
+const duration=document.querySelector('.time-duration')
+const fullScreenBtn=document.querySelector('.fullscreen')
 
 // Play & Pause ----------------------------------- //
+const togglePlay=()=>{
+    if(video.paused){
+        video.play()
 
+        playBtn.classList.replace('fa-play','fa-pause')
+    }else{
+        video.pause()
+
+        playBtn.classList.replace('fa-pause','fa-play')
+    }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +38,6 @@
 // Fullscreen ------------------------------- //
 
 
+// Event Listner
+playBtn.addEventListener('click',togglePlay)
+video.addEventListener('click',togglePlay)
